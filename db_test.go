06065bf2709f135166f06e1e3932de242ba183a2
Simon Hayward <simonhayward@users.noreply.github.com>
2026-01-31 16:29:46 +0000

    Add join test

--- db_test.go
+++ db_test.go
@@ -17,55 +17,50 @@ func TestDBInsert(t *testing.T) {
 	}
 	defer sqlite.db.Close()
 
-	expectedUser := User{
+	expectedUser, expectedToken := User{
 		UserID:  1,
 		Name:    "test",
 		Created: time.Now().UTC(),
+	}, Token{
+		Token:    "027864ad-ab88-4fbc-bedf-998d72bf33ac",
+		UserID:   1,
+		LastUsed: time.Now().UTC(),
+		Created:  time.Now().UTC(),
 	}
+
 	err = InsertRecord(sqlite.db, "user", &expectedUser)
 	if err != nil {
 		t.Fatal(err)
-	}
-
-	expectedToken := Token{
-		Token:    "027864ad-ab88-4fbc-bedf-998d72bf33ac",
-		UserID:   expectedUser.UserID,
-		LastUsed: time.Now().UTC(),
-		Created:  time.Now().UTC(),
 	}
 	err = InsertRecord(sqlite.db, "token", &expectedToken)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	var rawJSON []byte
-	err = sqlite.db.QueryRow("SELECT json(data) FROM user WHERE UserID = ?", expectedUser.UserID).Scan(&rawJSON)
+	var dataUser, dataToken []byte
+	err = sqlite.db.QueryRow(`
+		SELECT 
+		json(user.data), 
+		json(token.data) 
+		FROM user 
+		LEFT JOIN token ON user.userid = token.userid WHERE user.userid = ?`,
+		expectedUser.UserID).Scan(&dataUser, &dataToken)
 
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	var gotUser User
-	err = json.Unmarshal(rawJSON, &gotUser)
-	if err != nil {
-		t.Fatal(err)
+	var (
+		gotToken Token
+		gotUser  User
+	)
+	json.Unmarshal(dataUser, &gotUser)
+	json.Unmarshal(dataToken, &gotToken)
+
+	if !reflect.DeepEqual(expectedToken, gotToken) {
+		t.Errorf("Structs do not match!\nExpected: %+v\nGot: %+v", expectedToken, gotToken)
 	}
 	if !reflect.DeepEqual(expectedUser, gotUser) {
 		t.Errorf("Structs do not match!\nExpected: %+v\nGot: %+v", expectedUser, gotUser)
-	}
-
-	err = sqlite.db.QueryRow("SELECT json(data) FROM token WHERE token = ?", expectedToken.Token).Scan(&rawJSON)
-
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	var gotToken Token
-	err = json.Unmarshal(rawJSON, &gotToken)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if !reflect.DeepEqual(expectedToken, gotToken) {
-		t.Errorf("Structs do not match!\nExpected: %+v\nGot: %+v", expectedToken, gotToken)
 	}
 }
