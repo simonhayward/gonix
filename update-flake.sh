e8cefcf7b68e12e3e1cafca0fef77b752d81d0c1
Simon Hayward <simonhayward@users.noreply.github.com>
2026-02-10 20:30:05 +0000

    test run2 (#16)
    * single workflow

--- update-flake.sh
+++ update-flake.sh
@@ -4,7 +4,10 @@ set -eu
 
 OUT=$(mktemp -d -t nar-hash-XXX)
 go mod vendor -o "$OUT"
+
 # go install tailscale.com/cmd/nardump@v1.92.3
 SHA=$(nardump --sri "$OUT")
+
 sed -i "s|vendorHash = \".*\";|vendorHash = \"${SHA}\";|g" flake.nix
 rm -rf "$OUT"
+
