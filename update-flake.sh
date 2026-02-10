5acdd4ce3935a4b40cc00e6b78483200cc8c6198
Simon Hayward <simonhayward@users.noreply.github.com>
2026-02-10 19:00:57 +0000

    update flake file

---
+++ update-flake.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+set -eu
+
+OUT=$(mktemp -d -t nar-hash-XXX)
+go mod vendor -o "$OUT"
+# go install tailscale.com/cmd/nardump@v1.92.3
+SHA=$(nardump --sri "$OUT")
+sed -i "s|vendorHash = \".*\";|vendorHash = \"${SHA}\";|g" flake.nix
+rm -rf "$OUT"
