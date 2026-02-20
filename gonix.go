eeab2db9613d88a8056a05e47258e3b47c9dfd87
Simon Hayward <simonhayward@users.noreply.github.com>
2026-02-20 09:29:48 +0000

    add status handler to listen on 8080 (#21)

--- gonix.go
+++ gonix.go
@@ -2,7 +2,9 @@ package gonix
 
 import (
 	"fmt"
+	"io"
 	"log"
+	"net/http"
 	"os"
 	"path/filepath"
 
@@ -12,17 +14,30 @@ import (
 var sqlite *SQLiteDB
 
 func Run() error {
-	tmpdir, err := os.MkdirTemp("", "gonix_dev_*")
-	if err != nil {
-		return err
+	sqlitefile, exists := os.LookupEnv("SQLITE_PATH")
+	if exists == false || sqlitefile == "" {
+		tmpdir, err := os.MkdirTemp("", "gonix_dev_*")
+		if err != nil {
+			return err
+		}
+		defer os.RemoveAll(tmpdir)
+		sqlitefile = filepath.Join(tmpdir, "gonix.db")
 	}
-	defer os.RemoveAll(tmpdir)
 
-	sqlitefile := filepath.Join(tmpdir, "gonix.db")
-
-	if sqlite, err = NewSQLiteDB(sqlitefile, LogOptions, LogVersion); err != nil {
+	if _, err := NewSQLiteDB(sqlitefile, LogOptions, LogVersion); err != nil {
 		return fmt.Errorf("NewSQLiteDB(%s): %w", sqlitefile, err)
 	}
-	log.Printf("temp db: %s", sqlitefile)
+	log.Printf("db: %s", sqlitefile)
+
+	statusHandler := func(w http.ResponseWriter, req *http.Request) {
+		io.WriteString(w, "ok\n")
+	}
+
+	log.Println("Listening on :8080")
+	http.HandleFunc("/.status", statusHandler)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		return err
+	}
+
 	return nil
 }
